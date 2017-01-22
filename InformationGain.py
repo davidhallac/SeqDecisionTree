d440cadee5d14e7d49933778644ea922681bf2ce
David Hallac <david.hallac@gmail.com>
2017-01-22 09:26:22 -0800

    Information gain code

---
+++ InformationGain.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import os
+import math
+import sys
+
+#LOAD DATA
+posData = np.loadtxt('/dfs/scratch0/david/FengCollab/PositiveExamples.txt')
+
+print posData.shape
+
